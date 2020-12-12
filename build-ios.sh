558197deb064711cb8622b2c81a5558e95719505
Yun Hsiao Wu <inheritorofgalaxy@gmail.com>
2020-12-12 15:24:57 +0800

    minor

--- build-ios.sh
+++ build-ios.sh
@@ -1,7 +1,4 @@
 #!/bin/bash -x
 cmake .. -GXcode \
  -DCMAKE_SYSTEM_NAME=iOS \
- -DCMAKE_OSX_SYSROOT=iphoneos \
- -DTF_BUILD_BENCHMARKS=ON \
- -DOMPDIR=/Users/panda/StudyWork/Cocos/taskflow/omp-ios \
- -DTBBDIR=/Users/panda/StudyWork/Cocos/taskflow/tbb-ios
+ -DCMAKE_OSX_SYSROOT=iphoneos
